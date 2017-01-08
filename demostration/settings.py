cd41f0ac3cb9f37b2175085d8045f4584fa667a2
root <marcos_F192@hotmail.com>
2017-01-08 20:37:12 +0100

    mas cambios en settings.py

--- demostration/settings.py
+++ demostration/settings.py
@@ -17,8 +17,6 @@ import dj_database_url
 
 MESSAGE_LEVEL = message_constants.DEBUG
 
-CLIENT = MongoClient('mongodb://localhost:27017/')
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
